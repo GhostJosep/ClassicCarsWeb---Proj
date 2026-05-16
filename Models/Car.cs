f22b4a3d8f2811e550619b66c23332f7fb29b828
GhostJosep <thatmine123456789@gmail.com>
2026-05-16 12:18:45 +0800

    first commit

---
+++ Models/Car.cs
@@ -0,0 +1,13 @@
+namespace ClassicCarsWeb.Models
+{
+    public class Car
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Engine { get; set; }
+        public string Horsepower { get; set; }
+        public string FunFact { get; set; }
+    }
+}
